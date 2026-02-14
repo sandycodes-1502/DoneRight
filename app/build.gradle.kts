2529aad29bd1e912b396446795d34e3986b6de7f
sandycodes-1502 <torkadsandesh@gmail.com>
2026-02-14 15:42:37 +0530

    Release v1.1.3

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -16,8 +16,8 @@ android {
         applicationId = "com.sandycodes.doneright"
         minSdk = 26
         targetSdk = 36
-        versionCode = 2
-        versionName = "1.0.3"
+        versionCode = 3
+        versionName = "1.1.3"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
