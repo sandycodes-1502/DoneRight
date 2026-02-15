4e3b057891563d73a25d6183d092f897841e2f7c
sandycodes-1502 <torkadsandesh@gmail.com>
2026-02-15 17:13:22 +0530

    Release v1.1.6: Font error patch

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -16,8 +16,8 @@ android {
         applicationId = "com.sandycodes.doneright"
         minSdk = 26
         targetSdk = 36
-        versionCode = 4
-        versionName = "1.1.5"
+        versionCode = 5
+        versionName = "1.1.6"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
