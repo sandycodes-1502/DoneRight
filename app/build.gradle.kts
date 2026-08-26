38cd3385999e000a8afd7a94b9f5e259800e3bb4
sandycodes-1502 <torkadsandesh@gmail.com>
2026-08-26 18:23:31 +0530

    Fix: Scroll to top when new task added

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -16,8 +16,8 @@ android {
         applicationId = "com.sandycodes.doneright"
         minSdk = 26
         targetSdk = 36
-        versionCode = 5
-        versionName = "1.1.6"
+        versionCode = 6
+        versionName = "1.1.7"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
